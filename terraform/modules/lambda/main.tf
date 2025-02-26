1bd998314de2b7c87674a4e87a79e444efded642
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-26 23:47:08 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,9 +1,17 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
 resource "aws_lambda_function" "lambda_function" {
   function_name    = var.function_name
   runtime         = "python3.9"
   handler         = "lambda_function.lambda_handler"
   role            = var.role_arn
-  filename        = "lambda.zip"
+  filename        = data.archive_file.lambda_zip.output_path
+
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
