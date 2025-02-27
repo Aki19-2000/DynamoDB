8a5a03c127050228aff1096a430d03f7e163b979
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 10:42:35 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,19 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "dynamodb" {
-  source     = "./modules/dynamodb"
-  table_name = "serverless_workshop_intro"  # Provide the table name here
-  hash_key   = "Userid"  # Provide the hash key here
+  source      = "./modules/dynamodb"
+  table_name  = "serverless_workshop_intro"  # Provide the table name here
+  hash_key    = "Userid"                     # Provide the hash key here
 }
-
 
 module "iam" {
   source = "./modules/iam"
 }
 
 module "lambda" {
-  source      = "./modules/lambda"
+  source        = "./modules/lambda"
   function_name = "my_lambda_function"  # Provide the Lambda function name
-  role_arn     = "arn:aws:iam::123456789012:role/my-lambda-role"  # Provide the IAM role ARN for Lambda
-  table_name   = "serverless_workshop_intro"  # Provide the DynamoDB table name
+  role_arn      = module.iam.lambda_role_arn  # Reference the IAM role ARN output
+  table_name    = module.dynamodb.table_name  # Reference the DynamoDB table name
 }
-
 
 module "api_gateway" {
   source = "./modules/api_gateway"
