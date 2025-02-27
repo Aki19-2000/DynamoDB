49b0c8bf3c830652722e33b8b83328ad42f79b5a
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 12:24:55 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -17,9 +17,9 @@ resource "aws_api_gateway_method" "get_read_data" {
 }
 
 resource "aws_api_gateway_integration" "read_data_integration" {
-  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
-  resource_id = aws_api_gateway_resource.read_data.id
-  http_method = aws_api_gateway_method.get_read_data.http_method
+  rest_api_id             = aws_api_gateway_rest_api.serverless_api.id
+  resource_id            = aws_api_gateway_resource.read_data.id
+  http_method            = aws_api_gateway_method.get_read_data.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.read_data_lambda_arn}/invocations"
@@ -39,9 +39,9 @@ resource "aws_api_gateway_method" "post_insert_data" {
 }
 
 resource "aws_api_gateway_integration" "insert_data_integration" {
-  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
-  resource_id = aws_api_gateway_resource.insert_data.id
-  http_method = aws_api_gateway_method.post_insert_data.http_method
+  rest_api_id             = aws_api_gateway_rest_api.serverless_api.id
+  resource_id            = aws_api_gateway_resource.insert_data.id
+  http_method            = aws_api_gateway_method.post_insert_data.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.insert_data_lambda_arn}/invocations"
@@ -56,3 +56,6 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   stage_name  = "prod"
 }
 
+output "api_url" {
+  value = aws_api_gateway_deployment.api_deployment.invoke_url
+}
