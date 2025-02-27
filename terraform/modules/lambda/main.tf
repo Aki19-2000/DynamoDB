ab70b1bb21c5fcc12a5c6cd424e0ea9b54f9f9e4
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 09:43:27 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -11,3 +11,18 @@ resource "aws_lambda_function" "insert_data_lambda" {
     }
   }
 }
+
+resource "aws_lambda_function" "read_data_lambda" {
+  function_name = "read_data_lambda"
+  role          = aws_iam_role.lambda_dynamodb_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+  filename      = "lambda_functions/read_data.zip"  # Your zipped lambda file
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.serverless_workshop_intro.name
+    }
+  }
+}
+
