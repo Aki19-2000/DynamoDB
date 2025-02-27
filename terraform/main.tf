ae287303655cc780411aaffe55739cd2c291b113
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 09:44:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,24 +3,17 @@ provider "aws" {
 }
 
 module "dynamodb" {
-  source      = "./modules/dynamodb"
-  table_name  = "MyDynamoDBTable"
-  hash_key    = "_id"
+  source = "./modules/dynamodb"
 }
 
 module "iam" {
-  source      = "./modules/iam"
-  lambda_name = "bulk_insert_lambda"
+  source = "./modules/iam"
 }
 
 module "lambda" {
-  source        = "./modules/lambda"
-  function_name = "bulk_insert_lambda"
-  role_arn      = module.iam.lambda_role_arn
-  table_name    = module.dynamodb.table_name
+  source = "./modules/lambda"
 }
 
 module "api_gateway" {
-  source        = "./modules/api_gateway"
-  function_name = module.lambda.function_name
+  source = "./modules/api_gateway"
 }
