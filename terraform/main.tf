5251aa18e1b795cb040169507f6084f7f2fe1b81
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 10:31:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,11 @@ provider "aws" {
 }
 
 module "dynamodb" {
-  source = "./modules/dynamodb"
+  source     = "./modules/dynamodb"
+  table_name = "serverless_workshop_intro"  # Provide the table name here
+  hash_key   = "Userid"  # Provide the hash key here
 }
+
 
 module "iam" {
   source = "./modules/iam"
