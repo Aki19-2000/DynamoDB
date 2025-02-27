affed3d9535b373e8698deeb385ebdec5b29fbee
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 09:43:50 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -1,34 +1,40 @@
-resource "aws_api_gateway_rest_api" "api" {
-  name        = "ReadDynamoDBAPI"
-  description = "API to read data from DynamoDB"
+resource "aws_api_gateway_rest_api" "serverless_api" {
+  name        = "ServerlessAPI"
+  description = "API for interacting with DynamoDB"
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "data"
+resource "aws_api_gateway_resource" "read_data" {
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  parent_id   = aws_api_gateway_rest_api.serverless_api.root_resource_id
+  path_part   = "read_data"
 }
 
-resource "aws_api_gateway_method" "get_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
+resource "aws_api_gateway_method" "get_read_data" {
+  rest_api_id   = aws_api_gateway_rest_api.serverless_api.id
+  resource_id   = aws_api_gateway_resource.read_data.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.get_method.http_method
-
+resource "aws_api_gateway_integration" "read_data_integration" {
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  resource_id = aws_api_gateway_resource.read_data.id
+  http_method = aws_api_gateway_method.get_read_data.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_function.invoke_arn
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.read_data_lambda.arn}/invocations"
 }
 
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.read_data_integration
+  ]
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  stage_name  = "prod"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_function.function_name
+  function_name = aws_lambda_function.read_data_lambda.function_name
   principal     = "apigateway.amazonaws.com"
 }
