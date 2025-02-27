9200d6df6e5ba50aa7937bf3550ee34ed9fff69c
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 11:49:07 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,27 +1,27 @@
 resource "aws_lambda_function" "insert_data_lambda" {
-  function_name = var.function_name  # Use the function name variable
-  role          = var.role_arn       # Use the role ARN variable
+  function_name = var.function_name
+  role          = var.role_arn
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
-  filename      = "lambda_functions/${var.function_name}.zip"  # Dynamically reference the function name to get the correct zip file
+  filename      = "lambda_functions/insert_data.zip"
 
   environment {
     variables = {
-      TABLE_NAME = var.table_name  # Use the table name variable
+      TABLE_NAME = var.table_name
     }
   }
 }
 
 resource "aws_lambda_function" "read_data_lambda" {
-  function_name = var.function_name  # Use the function name variable
-  role          = var.role_arn       # Use the role ARN variable
+  function_name = var.function_name
+  role          = var.role_arn
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
-  filename      = "lambda_functions/${var.function_name}.zip"  # Dynamically reference the function name to get the correct zip file
+  filename      = "lambda_functions/read_data.zip"
 
   environment {
     variables = {
-      TABLE_NAME = var.table_name  # Use the table name variable
+      TABLE_NAME = var.table_name
     }
   }
 }
