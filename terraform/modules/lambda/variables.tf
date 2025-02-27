f050f53685816d544829b6213ec58cf695e5a135
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 10:43:16 +0530

    Update variables.tf

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,3 +1,14 @@
-variable "function_name" {}
-variable "role_arn" {}
-variable "table_name" {}
+variable "function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "The ARN of the IAM role to be assumed by Lambda"
+  type        = string
+}
+
+variable "table_name" {
+  description = "The name of the DynamoDB table"
+  type        = string
+}
