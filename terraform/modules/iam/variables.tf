c90c90b25d23b312d10a75e3ae73a3b851206291
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 12:09:46 +0530

    Update variables.tf

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -1,10 +1,15 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"  # Set the default region if needed
+  default     = "us-east-1"
 }
 
 variable "account_id" {
   description = "AWS Account ID"
+  type        = string
+}
+
+variable "table_name" {
+  description = "The name of the DynamoDB table"
   type        = string
 }
