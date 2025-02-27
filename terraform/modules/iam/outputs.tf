5f5be0cdf63616ca7ff43493b79c839a9ba53f91
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 10:46:22 +0530

    Update outputs.tf

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,3 +1,3 @@
 output "lambda_role_arn" {
-  value = aws_iam_role.lambda_dynamodb_role.arn  # Corrected the reference to the actual role
+  value = aws_iam_role.lambda_dynamodb_role.arn  # Ensure the correct role ARN is output
 }
