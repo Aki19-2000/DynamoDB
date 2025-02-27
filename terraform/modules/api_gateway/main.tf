54c38dc9bf4918a9a099e61bd3dcd612d64edc19
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 12:08:50 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -1,3 +1,21 @@
+resource "aws_api_gateway_rest_api" "serverless_api" {
+  name        = "ServerlessAPI"
+  description = "API for interacting with DynamoDB"
+}
+
+resource "aws_api_gateway_resource" "read_data" {
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  parent_id   = aws_api_gateway_rest_api.serverless_api.root_resource_id
+  path_part   = "read_data"
+}
+
+resource "aws_api_gateway_method" "get_read_data" {
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  resource_id = aws_api_gateway_resource.read_data.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "read_data_integration" {
   rest_api_id = aws_api_gateway_rest_api.serverless_api.id
   resource_id = aws_api_gateway_resource.read_data.id
@@ -7,10 +25,23 @@ resource "aws_api_gateway_integration" "read_data_integration" {
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.read_data_lambda_arn}/invocations"
 }
 
+resource "aws_api_gateway_resource" "insert_data" {
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  parent_id   = aws_api_gateway_rest_api.serverless_api.root_resource_id
+  path_part   = "insert_data"
+}
+
+resource "aws_api_gateway_method" "post_insert_data" {
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  resource_id = aws_api_gateway_resource.insert_data.id
+  http_method = "POST"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "insert_data_integration" {
   rest_api_id = aws_api_gateway_rest_api.serverless_api.id
-  resource_id = aws_api_gateway_resource.read_data.id
-  http_method = aws_api_gateway_method.get_read_data.http_method
+  resource_id = aws_api_gateway_resource.insert_data.id
+  http_method = aws_api_gateway_method.post_insert_data.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.insert_data_lambda_arn}/invocations"
@@ -23,4 +54,8 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   ]
   rest_api_id = aws_api_gateway_rest_api.serverless_api.id
   stage_name  = "prod"
+}
+
+output "api_url" {
+  value = aws_api_gateway_deployment.api_deployment.invoke_url
 }
