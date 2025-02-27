c6a43b1e5b115da4e44b928dd2cf39a741ad8562
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 11:49:48 +0530

    Update variables.tf

--- terraform/modules/api_gateway/variables.tf
+++ terraform/modules/api_gateway/variables.tf
@@ -2,3 +2,8 @@ variable "read_data_lambda_arn" {
   description = "The ARN of the read_data_lambda function"
   type        = string
 }
+
+variable "insert_data_lambda_arn" {
+  description = "The ARN of the insert_data_lambda function"
+  type        = string
+}
