f49ba5779ecf96d42ed05fcd8caa9442ad9a315c
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 10:56:47 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -10,9 +10,9 @@ resource "aws_api_gateway_resource" "read_data" {
 }
 
 resource "aws_api_gateway_method" "get_read_data" {
-  rest_api_id   = aws_api_gateway_rest_api.serverless_api.id
-  resource_id   = aws_api_gateway_resource.read_data.id
-  http_method   = "GET"
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  resource_id = aws_api_gateway_resource.read_data.id
+  http_method = "GET"
   authorization = "NONE"
 }
 
@@ -31,10 +31,4 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   ]
   rest_api_id = aws_api_gateway_rest_api.serverless_api.id
   stage_name  = "prod"
-}
-
-resource "aws_lambda_permission" "allow_api_gateway" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.read_data_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
 }
