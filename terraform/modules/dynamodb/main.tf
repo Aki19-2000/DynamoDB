1f7270dff09bf78101e8f1daf65b000edfb75719
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 10:30:04 +0530

    Update main.tf

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -1,9 +1,10 @@
 resource "aws_dynamodb_table" "serverless_workshop_intro" {
-  name           = "serverless_workshop_intro"
+  name           = var.table_name  # Use the table_name variable
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "Userid"
+  hash_key       = var.hash_key   # Use the hash_key variable
+
   attribute {
-    name = "Userid"
+    name = var.hash_key   # Use the hash_key variable for the attribute
     type = "S"
   }
 
