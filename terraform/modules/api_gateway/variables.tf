5e39f4a4c669412f7615f0d8d0af99608c2a09e2
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 12:19:22 +0530

    Update variables.tf

--- terraform/modules/api_gateway/variables.tf
+++ terraform/modules/api_gateway/variables.tf
@@ -7,3 +7,8 @@ variable "insert_data_lambda_arn" {
   description = "The ARN of the insert_data_lambda function"
   type        = string
 }
+variable "region" {
+  description = "The AWS region to deploy the resources"
+  type        = string
+}
+
