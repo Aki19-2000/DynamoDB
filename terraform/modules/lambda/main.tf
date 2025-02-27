e7286fd69649432827c8e2d18932e4c903163f63
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-27 10:45:23 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,28 +1,27 @@
 resource "aws_lambda_function" "insert_data_lambda" {
-  function_name = "insert_data_lambda"
-  role          = aws_iam_role.lambda_dynamodb_role.arn
+  function_name = var.function_name  # Use the function name variable
+  role          = var.role_arn       # Use the role ARN variable
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
   filename      = "lambda_functions/insert_data.zip"  # Your zipped lambda file
 
   environment {
     variables = {
-      TABLE_NAME = aws_dynamodb_table.serverless_workshop_intro.name
+      TABLE_NAME = var.table_name  # Use the table name variable
     }
   }
 }
 
 resource "aws_lambda_function" "read_data_lambda" {
-  function_name = "read_data_lambda"
-  role          = aws_iam_role.lambda_dynamodb_role.arn
+  function_name = var.function_name  # Use the function name variable
+  role          = var.role_arn       # Use the role ARN variable
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
   filename      = "lambda_functions/read_data.zip"  # Your zipped lambda file
 
   environment {
     variables = {
-      TABLE_NAME = aws_dynamodb_table.serverless_workshop_intro.name
+      TABLE_NAME = var.table_name  # Use the table name variable
     }
   }
 }
-
