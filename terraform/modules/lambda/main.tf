ee9ab6efbc53656e3528cb45424a3cc289968616
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-28 14:06:30 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,15 +1,39 @@
+resource "null_resource" "zip_insert_data_lambda" {
+  provisioner "local-exec" {
+    command = "zip terraform/modules/lambda/lambda_functions/insert_data.zip terraform/modules/lambda/insert_data.py"
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+resource "null_resource" "zip_read_data_lambda" {
+  provisioner "local-exec" {
+    command = "zip terraform/modules/lambda/lambda_functions/read_data_lambda.zip terraform/modules/lambda/read_data_lambda.py"
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
 resource "aws_lambda_function" "insert_data_lambda" {
   function_name = var.function_name
   role          = var.role_arn
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
-  filename      = "terraform/modules/lambda/lambda_functions/insert_data.zip"  # Correct path to the zip file
+  filename      = "terraform/modules/lambda/lambda_functions/insert_data.zip"  # Path to the zip file
 
   environment {
     variables = {
       TABLE_NAME = var.table_name
     }
   }
+
+  depends_on = [
+    null_resource.zip_insert_data_lambda
+  ]
 }
 
 resource "aws_lambda_function" "read_data_lambda" {
@@ -17,11 +41,15 @@ resource "aws_lambda_function" "read_data_lambda" {
   role          = var.role_arn
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
-  filename      = "terraform/modules/lambda/lambda_functions/read_data_lambda.zip"  # Correct path to the zip file
+  filename      = "terraform/modules/lambda/lambda_functions/read_data_lambda.zip"  # Path to the zip file
 
   environment {
     variables = {
       TABLE_NAME = var.table_name
     }
   }
+
+  depends_on = [
+    null_resource.zip_read_data_lambda
+  ]
 }
