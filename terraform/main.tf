8bbb4fe5638e5820dfa957b8401bddf96c81d42e
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-28 18:22:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,20 @@
+module "iam" {
+  source      = "./modules/iam"
+  table_name  = module.dynamodb.table_name
+  account_id  = "510278866235"  # Replace with your actual AWS account ID
+}
+
 module "insert_data_lambda" {
   source        = "./modules/lambda"
-  function_name = "insert_data_lambda"  # Unique function name for this Lambda
-  role_arn      = module.iam.lambda_role_arn
+  function_name = "insert_data_lambda"
+  role_arn      = module.iam.lambda_role_arn  # IAM role ARN from the IAM module
   table_name    = module.dynamodb.table_name
 }
 
 module "read_data_lambda" {
   source        = "./modules/lambda"
-  function_name = "read_data_lambda"  # Unique function name for this Lambda
-  role_arn      = module.iam.lambda_role_arn
+  function_name = "read_data_lambda"
+  role_arn      = module.iam.lambda_role_arn  # IAM role ARN from the IAM module
   table_name    = module.dynamodb.table_name
 }
 
