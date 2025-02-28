77691671d66c9c8d880c7027c396f8bd6381df13
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-28 13:31:27 +0530

    Update main.tf

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -3,14 +3,25 @@ resource "aws_dynamodb_table" "serverless_workshop_intro" {
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = var.hash_key   # Use the hash_key variable
 
+  # Primary key
   attribute {
     name = var.hash_key   # Use the hash_key variable for the attribute
     type = "S"
   }
 
+  # Attribute for _id
   attribute {
     name = "_id"
     type = "S"
+  }
+
+  # Global Secondary Index (GSI) for querying _id
+  global_secondary_index {
+    name               = "_id-index"
+    hash_key           = "_id"
+    projection_type    = "ALL"
+    read_capacity      = 5  # Optional, can be omitted for PAY_PER_REQUEST
+    write_capacity     = 5  # Optional, can be omitted for PAY_PER_REQUEST
   }
 
   tags = {
