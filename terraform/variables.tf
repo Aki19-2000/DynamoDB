183fc28a1a1b6661c946647ab4e305a320ef6b8e
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-28 12:56:48 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,12 @@
+# tflint-ignore: terraform_unused_declarations
 variable "region" {
+  type    = string
   default = "us-east-1"
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "account_id" {
+  type        = string
   description = "AWS Account ID"
   default     = "510278866235"
 }
