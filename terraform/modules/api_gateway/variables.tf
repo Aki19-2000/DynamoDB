8fa90f705294a76d24bed31c1767ee8c0ed6b274
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-01 18:03:19 +0530

    Update variables.tf

--- terraform/modules/api_gateway/variables.tf
+++ terraform/modules/api_gateway/variables.tf
@@ -1,3 +1,31 @@
+variable "api_name" {
+  description = "The name of the API Gateway"
+  type        = string
+  default     = "MyAPIGateway"
+}
+
+variable "api_stage" {
+  description = "The stage name for the API Gateway"
+  type        = string
+  default     = "prod"
+}
+
+variable "region" {
+  description = "The AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "read_data_lambda_arn" {
+  description = "ARN of the read data Lambda function"
+  type        = string
+}
+
+variable "insert_data_lambda_arn" {
+  description = "ARN of the insert data Lambda function"
+  type        = string
+}
+
 variable "read_data_lambda_name" {
   description = "Name of the read data Lambda function"
   type        = string
