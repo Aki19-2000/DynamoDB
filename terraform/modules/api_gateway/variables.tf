ba5494d09d44509e4effb47c2f98410e3ac4fce9
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-01 17:26:50 +0530

    Update variables.tf

--- terraform/modules/api_gateway/variables.tf
+++ terraform/modules/api_gateway/variables.tf
@@ -12,3 +12,8 @@ variable "insert_data_lambda_arn" {
   description = "The ARN of the insert data lambda function"
   type        = string
 }
+variable "api_stage" {
+  description = "The stage name for the API Gateway"
+  type        = string
+}
+
