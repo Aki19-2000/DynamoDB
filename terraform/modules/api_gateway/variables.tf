0c837816c3fb9a0d0433f27c1b68bf7bfcd02aa6
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-01 18:01:28 +0530

    Update variables.tf

--- terraform/modules/api_gateway/variables.tf
+++ terraform/modules/api_gateway/variables.tf
@@ -1,25 +1,9 @@
-variable "read_data_lambda_arn" {
-  description = "ARN for the Read Data Lambda function"
+variable "read_data_lambda_name" {
+  description = "Name of the read data Lambda function"
   type        = string
 }
 
-variable "insert_data_lambda_arn" {
-  description = "ARN for the Insert Data Lambda function"
+variable "insert_data_lambda_name" {
+  description = "Name of the insert data Lambda function"
   type        = string
-}
-
-variable "api_stage" {
-  description = "The stage name for the API Gateway"
-  type        = string
-}
-
-variable "region" {
-  description = "The AWS region"
-  type        = string
-}
-
-variable "api_name" {
-  description = "The name of the API Gateway"
-  type        = string
-  default     = "MyAPIGateway"
 }
