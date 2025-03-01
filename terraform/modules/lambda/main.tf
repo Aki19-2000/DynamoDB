b0f6c9b869cdc9e11a144e941eb39ae2bc008ab5
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-01 14:14:40 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -4,11 +4,6 @@ data "archive_file" "insert_data_zip" {
   output_path = "${path.module}/lambda_functions/insert_data.zip"
 }
 
-data "archive_file" "read_data_lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/lambda_functions1/read_data_lambda"
-  output_path = "${path.module}/lambda_functions1/read_data_lambda.zip"
-}
 
 resource "aws_lambda_function" "insert_data_lambda" {
   function_name = var.function_name
@@ -35,27 +30,3 @@ resource "aws_lambda_function" "insert_data_lambda" {
   }
 }
 
-resource "aws_lambda_function" "read_data_lambda" {
-  function_name = var.function_name
-  role          = var.role_arn
-  handler       = "index.lambda_handler"
-  runtime       = "python3.8"
-  filename      = data.archive_file.read_data_lambda_zip.output_path
-
-  environment {
-    variables = {
-      TABLE_NAME = var.table_name
-    }
-  }
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes = [
-      function_name,
-      role,
-      handler,
-      runtime,
-      filename,
-    ]
-  }
-}
