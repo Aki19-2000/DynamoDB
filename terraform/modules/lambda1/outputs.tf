9ba4fe220ca5c2f1808a1fcb9b16e995ca300afe
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-01 14:15:22 +0530

    Update outputs.tf

--- terraform/modules/lambda1/outputs.tf
+++ terraform/modules/lambda1/outputs.tf
@@ -1,7 +1,3 @@
-output "insert_data_lambda_arn" {
-  value = aws_lambda_function.insert_data_lambda.arn
-}
-
 output "read_data_lambda_arn" {
-  value = aws_lambda_function.read_data_lambda.arn
+  value = aws_lambda_function.read_data.arn
 }
