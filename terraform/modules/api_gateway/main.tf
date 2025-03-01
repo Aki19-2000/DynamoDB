08d65c51032bfabb1b46d193babfbb510822df61
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-01 17:29:06 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -1,15 +1,17 @@
+# Define the API Gateway REST API
 resource "aws_api_gateway_rest_api" "this" {
   name        = "${var.lambda_function_name}-api"
   description = "API Gateway to trigger ${var.lambda_function_name}"
 }
 
+# Create a resource path for '/invoke'
 resource "aws_api_gateway_resource" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "invoke"
 }
 
-# GET Method
+# GET Method - Allow GET requests on /invoke
 resource "aws_api_gateway_method" "get_method" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   resource_id   = aws_api_gateway_resource.this.id
@@ -17,6 +19,7 @@ resource "aws_api_gateway_method" "get_method" {
   authorization = "NONE"
 }
 
+# Integration for GET method with Lambda
 resource "aws_api_gateway_integration" "get_integration" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_resource.this.id
@@ -26,7 +29,7 @@ resource "aws_api_gateway_integration" "get_integration" {
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.this.arn}/invocations"
 }
 
-# POST Method
+# POST Method - Allow POST requests on /invoke
 resource "aws_api_gateway_method" "post_method" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   resource_id   = aws_api_gateway_resource.this.id
@@ -34,6 +37,7 @@ resource "aws_api_gateway_method" "post_method" {
   authorization = "NONE"
 }
 
+# Integration for POST method with Lambda
 resource "aws_api_gateway_integration" "post_integration" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_resource.this.id
@@ -43,7 +47,7 @@ resource "aws_api_gateway_integration" "post_integration" {
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.this.arn}/invocations"
 }
 
-# Lambda permissions for API Gateway to invoke the function
+# Lambda permissions for API Gateway to invoke the Lambda function
 resource "aws_lambda_permission" "this" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.this.function_name
@@ -53,7 +57,7 @@ resource "aws_lambda_permission" "this" {
 # Deploy API Gateway to a stage
 resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
-  stage_name  = var.api_stage
+  stage_name  = var.api_stage  # Use the 'api_stage' variable for the stage name
 
   depends_on = [
     aws_api_gateway_integration.get_integration,
@@ -61,4 +65,9 @@ resource "aws_api_gateway_deployment" "this" {
     aws_api_gateway_integration.post_integration,
     aws_api_gateway_method.post_method
   ]
+}
+
+# Output the API Gateway URL (optional)
+output "api_url" {
+  value = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${var.region}.amazonaws.com/${var.api_stage}/invoke"
 }
