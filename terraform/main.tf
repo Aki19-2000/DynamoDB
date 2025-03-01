bb0d2fb0e8092773e8d585fe3d2f4c73bb93ac66
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-01 14:16:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ module "insert_data_lambda" {
 
 module "read_data_lambda" {
   source        = "./modules/lambda1"
-  function_name = "read_data_lambda"
+  function_name = "read_data"
   workspace     = terraform.workspace  # Pass the current workspace
   role_arn      = module.iam.lambda_role_arn
   table_name    = module.dynamodb.table_name
