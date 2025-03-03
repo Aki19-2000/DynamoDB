4a203babfbc6a2e50d628b2d7bc7dd07447ab00d
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-03 10:59:36 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -36,15 +36,12 @@ resource "aws_lambda_permission" "read_data_permission" {
   principal     = "apigateway.amazonaws.com"
 }
 
-# Create API Gateway Deployment
 resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
 
   depends_on = [
     aws_api_gateway_integration.get_integration,
-    aws_api_gateway_method.get_method,
-    aws_api_gateway_integration.post_integration,
-    aws_api_gateway_method.post_method
+    aws_api_gateway_method.get_method
   ]
 }
 
