a656238ab610fc8806b5348ab2ce0f5d38072784
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-03 10:37:11 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -29,41 +29,10 @@ resource "aws_api_gateway_integration" "get_integration" {
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.read_data_lambda_arn}/invocations"
 }
 
-# Create a resource path for '/insert_data'
-resource "aws_api_gateway_resource" "insert_data" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  path_part   = "insert_data"
-}
-
-# POST Method - Allow POST requests on /insert_data
-resource "aws_api_gateway_method" "post_method" {
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.insert_data.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-# Integration for POST method with Lambda (insert data)
-resource "aws_api_gateway_integration" "post_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.this.id
-  resource_id             = aws_api_gateway_resource.insert_data.id
-  http_method             = aws_api_gateway_method.post_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.insert_data_lambda_arn}/invocations"
-}
-
 # Lambda permissions for API Gateway to invoke the Lambda function
 resource "aws_lambda_permission" "read_data_permission" {
   action        = "lambda:InvokeFunction"
   function_name = var.read_data_lambda_arn
-  principal     = "apigateway.amazonaws.com"
-}
-
-resource "aws_lambda_permission" "insert_data_permission" {
-  action        = "lambda:InvokeFunction"
-  function_name = var.insert_data_lambda_arn
   principal     = "apigateway.amazonaws.com"
 }
 
