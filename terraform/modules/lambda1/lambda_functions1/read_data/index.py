99bd4930285b1e83ecae4070967d479be84de336
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-03 10:38:32 +0530

    Update index.py

--- terraform/modules/lambda1/lambda_functions1/read_data/index.py
+++ terraform/modules/lambda1/lambda_functions1/read_data/index.py
@@ -1,16 +1,20 @@
-
 import boto3
 import os
+import json  # <-- Add this line to import the json module
 
 def lambda_handler(event, context):
     table_name = os.environ['TABLE_NAME']
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(table_name)
 
-    # Scan the table (consider using Query for better performance with large tables)
+    # Scan the table
     response = table.scan()
 
+    # Return the correct format for AWS_PROXY integration
     return {
         'statusCode': 200,
-        'body': response['Items']
+        'body': json.dumps(response['Items']),  # Use json.dumps to convert the list into a JSON string
+        'headers': {
+            'Content-Type': 'application/json'
+        }
     }
